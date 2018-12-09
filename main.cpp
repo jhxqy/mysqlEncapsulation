4f276b9511e38c155655210c5a57be929decac42
jhxqy <1139648416@qq.com>
2018-12-09 14:35:10 +0800

    .

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mysql2
+//
+//  Created by 贾皓翔 on 2018/12/9.
+//  Copyright © 2018 贾皓翔. All rights reserved.
+//
+
+#include <iostream>
+#include <mysql.h>
+int main(int argc, const char * argv[]) {
+    mysql_init(NULL);
+    return 0;
+}
